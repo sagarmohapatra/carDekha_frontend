4fc61520432453bb14b4651685e9c27396314709
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2024-02-23 00:35:17 +0530

    home page

---
+++ src/component/constant/constant.js
@@ -0,0 +1,97 @@
+export const firstImg = [
+  {
+    id: "1",
+    url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Mahindra/Thar/10745/1697697308167/front-left-side-47.jpg",
+    name: "Mahindra Thar",
+    price: "13.59 - 17.35 Lakh*",
+    offer: "Check February Offers",
+  },
+  {
+    id: "2",
+
+    url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Hyundai/Creta/8667/1705465218824/front-left-side-47.jpg",
+    name: "Hyundai Creta",
+    price: "8.15 - 15.60 Lakh*",
+    offer: "Check February Offers",
+  },
+  {
+    id: "3",
+
+    url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Mahindra/Scorpio/10764/1708522826716/front-left-side-47.jpg",
+    name: "Mahindra Scrorpia",
+    price: "6.13 - 10.20 Lakh*",
+    offer: "Check February Offers",
+  },
+  {
+    id: "4",
+
+    url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Tata/Nexon/11104/1697698470038/front-left-side-47.jpg",
+    name: "Tata Nexon",
+    price: "8.34 - 14.14 Lakh*",
+    offer: "Check February Offers",
+  },
+  {
+    id: "5",
+    url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Tata/Punch/10681/1691392713058/front-left-side-47.jpg",
+    name: "Tata Punch",
+    price: "11 - 20.15 Lakh*",
+    offer: "Check February Offers",
+  },
+  {
+    id: "6",
+    url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Maruti/Brezza/10388/1694424068944/front-left-side-47.jpg",
+    name: "Maruti Brezza",
+    price: "11.25 - 17.20 Lakh*",
+    offer: "Check February Offers",
+  },
+];
+
+
+export const secondImg = [
+    {
+      id: "1",
+      url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Maruti/Swift/10406/1697698080681/front-left-side-47.jpg",
+      name: "Maruti Swift",
+      price: "5.99 - 9.03 Lakh*",
+      offer: "Check February Offers",
+    },
+    {
+      id: "2",
+  
+      url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Maruti/Baleno/10497/1697697558001/front-left-side-47.jpg",
+      name: "Maruti Boleno",
+      price: "8.15 - 15.60 Lakh*",
+      offer: "Check February Offers",
+    },
+    {
+      id: "3",
+  
+      url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Tata/Tiago/10655/1706791879838/front-left-side-47.jpg",
+      name: "Tata Tiago",
+      price: "6.13 - 10.20 Lakh*",
+      offer: "Check February Offers",
+    },
+    {
+      id: "4",
+  
+      url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Maruti/Wagon-R/10365/1687580655855/front-left-side-47.jpg",
+      name: "Maruti Warnor",
+      price: "8.34 - 14.14 Lakh*",
+      offer: "Check February Offers",
+    },
+    {
+      id: "5",
+      url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Hyundai/i20/9471/1697696007962/front-left-side-47.jpg",
+      name: "Hyndai i20",
+      price: "11 - 20.15 Lakh*",
+      offer: "Check February Offers",
+    },
+    {
+      id: "6",
+      url: "https://stimg.cardekho.com/images/carexteriorimages/630x420/Tata/Altroz/10707/1706784476472/front-left-side-47.jpg",
+      name: "Tata alroze",
+      price: "11.25 - 17.20 Lakh*",
+      offer: "Check February Offers",
+    },
+  ];
+  
