d1e107e1406bd8ac7c31ef011638ab04ad8eaa19
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2024-03-06 20:00:41 +0530

    add value

--- src/component/home/Compare.jsx
+++ src/component/home/Compare.jsx
@@ -11,10 +11,11 @@ import {
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
-import { fifthImg, newly, brand } from "../constant/constant";
+import { fifthImg, newly, brand, compare } from "../constant/constant";
 import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 import Container from "@mui/material/Container";
 import styled from "@emotion/styled";
+import AddIcon from "@mui/icons-material/Add";
 
 import "react-multi-carousel/lib/styles.css";
 import Brand from "./Brand";
@@ -39,6 +40,23 @@ const News = styled(Button)({
   color: "black",
   textTransform: "none",
   fontWeight: "600",
+});
+
+const Vs = styled(Box)({
+  color: "white",
+  backgroundColor: "black",
+  width: "37px",
+  height: "37px",
+  marginLeft: "5px",
+  textAlign: "center",
+  borderRadius: "20px",
+  alignItems: "center",
+  display: "flex",
+  justifyContent: "center",
+  fontSize: "20px",
+  position: "relative",
+  marginLeft: "170px",
+  marginTop: "50px",
 });
 
 const responsive = {
@@ -68,34 +86,100 @@ const Compare = () => {
         <CardContent>
           <Head>Compare to buy the right car</Head>
 
-          <Box style={{ marginTop: "35px", marginBottom: "30px" }}>
+          <Box style={{ marginTop: "25px", marginBottom: "20px" }}>
             <Carousel
               responsive={responsive}
               swipeable={false}
               draggable={false}
             >
-              {fifthImg.map((data) => (
-                <Card key={data.id} style={{ width: "275px" }}>
-                  <CardMedia
-                    component="img"
-                    style={{ height: "190px", width: "275px" }}
-                    src={data.url}
-                    alt="Paella dish"
-                  />
+              {compare.map((data) => (
+                <Card
+                  key={data.url1}
+                  style={{
+                    width: "375px",
+                    marginBottom: "15px",
+                    height: "290px",
+                  }}
+                >
+                  <Box style={{ display: "flex", position: "absolute" }}>
+                    <Box>
+                      <CardMedia
+                        component="img"
+                        style={{ height: "140px", width: "187px" }}
+                        src={data.url1}
+                        alt="Paella dish"
+                      />
+                    </Box>
+                    <Box>
+                      <CardMedia
+                        component="img"
+                        style={{ height: "140px", width: "188px" }}
+                        src={data.url2}
+                        alt="Paella dish"
+                      />
+                    </Box>
+                  </Box>
+                  <Vs>vs</Vs>
+
+                  <Box style={{ display: "flex" }}>
+                    <Box style={{ marginTop: "60px", marginLeft: "5px" }}>
+                      <Typography
+                        style={{ fontSize: "14px", color: "#6f6f6f" }}
+                      >
+                        {data.brand1}
+                      </Typography>
+                      <Typography
+                        style={{
+                          color: "black",
+                          fontSize: "16px",
+                          fontWeight: "500",
+                        }}
+                      >
+                        {data.model1}
+                      </Typography>
+                      <Typography
+                        style={{
+                          fontSize: "17px",
+                          display: "flex",
+                          lineHeight: "24px",
+                        }}
+                      >
+                        <span>
+                          <Rupe />
+                        </span>
+                        {data.price1}
+                      </Typography>
+                    </Box>
+                    <Box style={{ marginTop: "60px", marginLeft: "50px" }}>
+                      <Typography
+                        style={{ fontSize: "14px", color: "#6f6f6f" }}
+                      >
+                        {data.brand2}
+                      </Typography>
+                      <Typography
+                        style={{
+                          color: "black",
+                          fontSize: "16px",
+                          fontWeight: "500",
+                        }}
+                      >
+                        {data.model2}
+                      </Typography>
+                      <Typography
+                        style={{
+                          fontSize: "17px",
+                          display: "flex",
+                          lineHeight: "24px",
+                        }}
+                      >
+                        <span>
+                          <Rupe />
+                        </span>
+                        {data.price2}
+                      </Typography>
+                    </Box>
+                  </Box>
                   <Box style={{ marginLeft: "15px" }}>
-                    <Typography
-                      style={{
-                        fontSize: "15px",
-                        fontWeight: "400",
-                        marginTop: "15px",
-                      }}
-                    >
-                      {data.name}
-                    </Typography>
-                    <Typography style={{ fontSize: "15px", marginTop: "3px" }}>
-                      <Rupe />
-                      {data.price}
-                    </Typography>
                     <Box>
                       <Button
                         variant="outlined"
@@ -107,16 +191,66 @@ const Compare = () => {
                           borderColor: "#f75d34",
                           color: "#f75d34",
                           textTransform: "none",
-                          width: "240px",
+                          width: "245px",
+                          marginLeft: "40px",
                         }}
                       >
-                        {data.offer}
+                        {data.compare}
                       </Button>
                     </Box>
                   </Box>
                 </Card>
               ))}
+              <Card
+                style={{
+                  width: "375px",
+                  marginBottom: "15px",
+                  height: "290px",
+                }}
+              >
+                <Box
+                  style={{
+                    width: "72px",
+                    height: "72px",
+                    border: "1px dashed",
+                    marginLeft: "65px",
+                    marginTop: "60px",
+                    borderRadius: "50%",
+                    position: "relative",
+                    cursor: "pointer",
+                    cursor: "pointer",
+                  }}
+                >
+                  <AddIcon
+                    style={{
+                      height: "50px",
+                      fontSize: "40px",
+                      color: "#b7b8b9",
+                      position: "absolute",
+                      marginLeft: "16px",
+                      marginTop: "10px",
+                    }}
+                  />
+                </Box>
+                <Typography style={{ marginLeft: "70px" }}>Add Car</Typography>
+                <Divider orientation="vertical" flexItem/>
+              </Card>
             </Carousel>
+          </Box>
+          <Divider />
+          <Box style={{ marginTop: "20px", marginLeft: "2px" }}>
+            <Typography>
+              <a
+                style={{
+                  textDecoration: "none",
+                  color: "#2176ae",
+                  fontSize: "14px",
+                }}
+                href="https://www.cardekho.com/best-suv"
+              >
+                View all Car Visual Stories
+              </a>
+            </Typography>
           </Box>
         </CardContent>
       </Card>
